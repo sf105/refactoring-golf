3af2551753a32598a2803873eba916ee6664468c
sf105 <github@m3p.co.uk>
2018-11-15 12:03:53 +0000

    tax at lower rate

--- src/payroll/PayslipTest.java
+++ src/payroll/PayslipTest.java
@@ -7,8 +7,15 @@ import static org.junit.Assert.assertEquals;
 public class PayslipTest {
     @Test public void
     no_tax_for_salary_below_threshold() {
-        assertEquals(500, new Payslip(500).netSalary());
+        assertEquals(5000, new Payslip(5000).netSalary());
     }
+
+    @Test public void
+    low_rate_tax_for_salary_in_lower_rate_band() {
+        final long netSalary = 5000 + 13500;
+        assertEquals(netSalary, new Payslip(20000).netSalary());
+    }
+
 
     public static class Payslip {
         private final long grossSalary;
@@ -18,7 +25,11 @@ public class PayslipTest {
         }
 
         public long netSalary() {
-            return grossSalary;
+            if (grossSalary <= 5000) return grossSalary;
+
+            long taxable = grossSalary - 5000;
+
+            return grossSalary - Math.round(taxable * 0.1);
         }
     }
 }
