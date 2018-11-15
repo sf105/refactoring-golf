2279a5195131bf2d25561be2d967eef4316d103d
sf105 <github@m3p.co.uk>
2018-11-15 16:40:55 +0000

    extract method object TaxCalculator

--- src/payroll/Payslip.java
+++ src/payroll/Payslip.java
@@ -8,29 +8,34 @@ public class Payslip {
     }
 
     public long netSalary() {
-        return grossSalary - taxFor(grossSalary);
+        return grossSalary - new TaxCalculator().invoke();
     }
 
-    private long taxFor(long grossSalary) {
-        final TaxBand lowerTaxBand = new TaxBand.ChainedTaxBand(0.1, 5000, 1500, TaxBand.NIL_TAX_BAND);
-        final TaxBand middleTaxBand = new TaxBand.ChainedTaxBand(0.2, 20000, 4000, lowerTaxBand);
-        final TaxBand upperTaxBand = new TaxBand.ChainedTaxBand(0.4, 40000, 0, middleTaxBand);
-
-
-        long tax = 0;
-
-        if (lowerTaxBand.appliesTo(grossSalary)) {
-            tax = lowerTaxBand.taxFor(grossSalary);
+    private class TaxCalculator {
+        public long invoke() {
+            return taxFor(grossSalary);
         }
 
-        if (middleTaxBand.appliesTo(grossSalary)) {
-            tax = middleTaxBand.taxFor(grossSalary);
-        }
+        private long taxFor(long grossSalary) {
+            final TaxBand lowerTaxBand = new TaxBand.ChainedTaxBand(0.1, 5000, 1500, TaxBand.NIL_TAX_BAND);
+            final TaxBand middleTaxBand = new TaxBand.ChainedTaxBand(0.2, 20000, 4000, lowerTaxBand);
+            final TaxBand upperTaxBand = new TaxBand.ChainedTaxBand(0.4, 40000, 0, middleTaxBand);
 
-        if (upperTaxBand.appliesTo(grossSalary)) {
-            tax =  upperTaxBand.taxFor(grossSalary);
+
+            long tax = 0;
+
+            if (lowerTaxBand.appliesTo(grossSalary)) {
+                tax = lowerTaxBand.taxFor(grossSalary);
+            }
+
+            if (middleTaxBand.appliesTo(grossSalary)) {
+                tax = middleTaxBand.taxFor(grossSalary);
+            }
+
+            if (upperTaxBand.appliesTo(grossSalary)) {
+                tax =  upperTaxBand.taxFor(grossSalary);
+            }
+            return tax;
         }
-        return tax;
     }
-
 }
