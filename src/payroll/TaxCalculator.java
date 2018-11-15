3cc170ecd6c1e478d59f6579d7e452e995bed906
sf105 <github@m3p.co.uk>
2018-11-15 16:52:10 +0000

    use clever streams stuff to find right tax band

--- src/payroll/TaxCalculator.java
+++ src/payroll/TaxCalculator.java
@@ -1,18 +1,20 @@
 package payroll;
 
+import java.util.stream.Stream;
+
+import static payroll.TaxBand.NIL_TAX_BAND;
+
 public class TaxCalculator {
-    private final TaxBand lowerTaxBand = new TaxBand.ChainedTaxBand(0.1, 5000, 1500, TaxBand.NIL_TAX_BAND);
+    private final TaxBand lowerTaxBand = new TaxBand.ChainedTaxBand(0.1, 5000, 1500, NIL_TAX_BAND);
     private final TaxBand middleTaxBand = new TaxBand.ChainedTaxBand(0.2, 20000, 4000, lowerTaxBand);
     private final TaxBand upperTaxBand = new TaxBand.ChainedTaxBand(0.4, 40000, 0, middleTaxBand);
+    private final Stream<TaxBand> taxBands = Stream.of(upperTaxBand, middleTaxBand, lowerTaxBand);
 
     public long taxFor(long grossSalary) {
         return taxBandFor(grossSalary).taxFor(grossSalary);
     }
 
     private TaxBand taxBandFor(long grossSalary) {
-        if (upperTaxBand.appliesTo(grossSalary)) return upperTaxBand;
-        if (middleTaxBand.appliesTo(grossSalary)) return middleTaxBand;
-        if (lowerTaxBand.appliesTo(grossSalary)) return  lowerTaxBand;
-        return TaxBand.NIL_TAX_BAND;
+        return taxBands.filter(taxBand -> taxBand.appliesTo(grossSalary)).findFirst().orElse(NIL_TAX_BAND);
     }
 }
