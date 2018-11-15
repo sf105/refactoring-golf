e0dbbae8239a7b899cb869aa42d048a84a223c7b
sf105 <github@m3p.co.uk>
2018-11-15 12:02:08 +0000

    no tax below first threshold

--- src/payroll/PayslipTest.java
+++ src/payroll/PayslipTest.java
@@ -1,4 +1,24 @@
 package payroll;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PayslipTest {
+    @Test public void
+    no_tax_for_salary_below_threshold() {
+        assertEquals(500, new Payslip(500).netSalary());
+    }
+
+    public static class Payslip {
+        private final long grossSalary;
+
+        public Payslip(long grossSalary) {
+            this.grossSalary = grossSalary;
+        }
+
+        public long netSalary() {
+            return grossSalary;
+        }
+    }
 }
