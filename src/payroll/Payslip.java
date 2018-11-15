21d3fd738e1e5194d496ec58ea1a6998c0ef4458
sf105 <github@m3p.co.uk>
2018-11-15 16:46:28 +0000

    move TaxCalculator to upper level, make public

--- src/payroll/Payslip.java
+++ src/payroll/Payslip.java
@@ -13,21 +13,4 @@ public class Payslip {
         return grossSalary - taxCalculator.taxFor(grossSalary);
     }
 
-    private static class TaxCalculator {
-
-        private final TaxBand lowerTaxBand = new TaxBand.ChainedTaxBand(0.1, 5000, 1500, TaxBand.NIL_TAX_BAND);
-        private final TaxBand middleTaxBand = new TaxBand.ChainedTaxBand(0.2, 20000, 4000, lowerTaxBand);
-        private final TaxBand upperTaxBand = new TaxBand.ChainedTaxBand(0.4, 40000, 0, middleTaxBand);
-
-        private long taxFor(long grossSalary) {
-            return taxBandFor(grossSalary).taxFor(grossSalary);
-        }
-
-        private TaxBand taxBandFor(long grossSalary) {
-            if (upperTaxBand.appliesTo(grossSalary)) return upperTaxBand;
-            if (middleTaxBand.appliesTo(grossSalary)) return middleTaxBand;
-            if (lowerTaxBand.appliesTo(grossSalary)) return  lowerTaxBand;
-            return TaxBand.NIL_TAX_BAND;
-        }
-    }
 }
